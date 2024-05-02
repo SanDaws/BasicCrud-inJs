19ba4172c76501c443026aa860da9be999786927
Daws <castrogiraldosantiago@gmail.com>
2024-05-01 21:36:55 -0500

    create works

--- src/js/main.js
+++ src/js/main.js
@@ -22,53 +22,10 @@ function Pet(petId,petName,specie,breed,weight,condition,bdayDate,owner) {
     this.age=age(this.bdayDate)
     this.owners=owner// for now on we are going to use only one owner, later we will use a set or an array
 }
-//create
-
-function createOwner(identification,petId) {
-    
-    while ((identification==0)) {
-        identification=prompt('Dijite una identificacion')
-        if (Number.isNaN(parseInt(identification))==true) {
-            alert('Numero de identificacion No valido')
-            identification=0
-        }
-    
-    }
-
-    let name=prompt('Nombre del dueño')
-    let phone=prompt('Telefono del dueño')
-    let email=prompt('Correo electronico')
-    let pets=[]
-    if (petId==0) {
-        createPet()
-    }else{
-        pets.push(petId)
-    }
-
-    const owner=new Owner(identification,name,phone,email,pets)
-    owners.push(owner)
-    alert('Registro Satisfactorio')
-}
-
-function createPet() {
-    let petId=petId()
-    let petName=prompt('nombre de la mascota:').toLowerCase()
-    let specie= prompt('A que especie pertenece', 'Canino , felino, ave... etc').toLowerCase()
-    let breed=prompt('Raza del animal',).toLowerCase()
-    let weight=weightValidation()
-    let condition=conditionStrtoBool() 
-    let bday=new Date(prompt('Fecha de naciemiento (formato: AAAA/MM/DD) ','AAAA/MM/DD'))
-    let owner =ownerValidator(petId)
-    
-    let pet= new Pet(petId,petName,specie,breed,weight,condition,bday,owner)
-    
-    pets.push(pet)
-    alert('Cliente Creado satisfactoriamente')
-}
 
 //calculation
 
-
+//age calculator
 function age(bday) {
     let bdayDate= bday
     let ActualDay = new Date();
@@ -82,7 +39,7 @@ function age(bday) {
 
     return age;
 } 
-
+//pet id generator
 function petId() { //Create a new Integer based on a date and the length of pets[]
     const actDay= new Date()
     // basic template for new pet: MMYYYYDDINDEX
@@ -97,6 +54,30 @@ function weightValidation() {// returns the weifgh
     data= (Number.isNaN(parseInt(data))==false)? parseInt(data):weightValidation() // if weight different for NaN, return a wieght in number, else strarts a recursive loop 
     return data
 } 
+//let the user chose betwen the register of an owner or a pet
+function HumanOAnimalSelector(operacion) {
+    let selec =prompt(`Desea ${operacion} el registro de:
+    1:Mascota
+    2:Cliente
+    b:atras`).toLowerCase()
+    switch (selec) {
+        case '1': // pet 
+            return 0
+
+        case '2'://owner
+        return  1
+
+        case 'b':
+            retunMenu()
+        break;
+        default:
+            console.error('No opcion seleccionable')
+            HumanOAnimalSelector();
+            break;
+    }
+    
+}
+
 //param=(OwnerID:Number, PetId: )
 function ownerValidator(petId) { // returs id or a the creation of a new owner
      // i can do a generic validator but it will cost a lot of time, future versions will use it
@@ -130,4 +111,112 @@ function conditionBooltoStr(condition) { //returns the strings values of healtin
     return (condition==true)? 'Estable':'Critico' 
 } 
 
+//create
+
+function createOwner(identification,petId) {
+    
+    while ((identification==0)) {
+        identification=prompt('Dijite una identificacion')
+        if (Number.isNaN(parseInt(identification))==true) {
+            alert('Numero de identificacion No valido')
+            identification=0
+        }
+    
+    }
+
+    let name=prompt('Nombre del dueño')
+    let phone=prompt('Telefono del dueño')
+    let email=prompt('Correo electronico')
+    let pets=[]
+    if (petId==0) {
+        createPet()
+    }else{
+        pets.push(petId)
+    }
+
+    const owner=new Owner(identification,name,phone,email,pets)
+    owners.push(owner)
+    alert('Registro Satisfactorio')
+}
+
+function createPet() {
+    let id=petId()
+    let petName=prompt('nombre de la mascota:').toLowerCase()
+    let specie= prompt('A que especie pertenece', 'Canino , felino, ave... etc').toLowerCase()
+    let breed=prompt('Raza del animal',).toLowerCase()
+    let weight=weightValidation()
+    let condition=conditionStrtoBool() 
+    let bday=new Date(prompt('Fecha de naciemiento (formato: AAAA/MM/DD) ','AAAA/MM/DD'))
+    let owner =ownerValidator(petId)
+    
+    let pet= new Pet(id,petName,specie,breed,weight,condition,bday,owner)
+    
+    pets.push(pet)
+    alert('Cliente Creado satisfactoriamente')
+}
+
+
+
 //templates
+
+
+
+//Main Menu
+function MainMenu() {
+    let input= prompt(`bienvenido al menu, elija su opcion
+    1: Crear un nuevo registro
+    2: buscar en la base datos(mascota o propietario)
+    3: Acrtualizar registro
+    4: Eliminar registro
+    Escribe "s" para salir
+    `).toLowerCase()
+    switch (input) {
+        case '1'://Create
+        if(HumanOAnimalSelector('crear')==0){// function returns:0, pet option
+            createPet()
+        }else{//function returns :1, owner option
+            createOwner(0,0)
+        }
+        
+        case '2'://read
+        
+        if(HumanOAnimalSelector('Listar')==0){// function returns:0, pet option
+            listPets()
+        }else{//function returns :1, owner option
+            listOwners()
+        }
+        break;
+
+
+        case '3'://update
+        
+        if(HumanOAnimalSelector('actualizar')==0){// function returns:0, pet option
+            
+        }else{//function returns :1, owner option
+
+        }
+        break;
+
+        
+        case '4'://Delete
+        
+        if(HumanOAnimalSelector('eliminar')==0){// function returns:0, pet option
+            
+        }else{//function returns :1, owner option
+
+        }
+        break;
+
+        case 's'://exit
+        return 0
+
+        default:
+            console.error('No opcion seleccionable')
+            MainMenu()
+            break;
+    }
+    
+}
+
+// Execution
+MainMenu()
